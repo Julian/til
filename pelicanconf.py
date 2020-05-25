ab9e33cd8cf6492f72c2585d0949f9272c291975
Julian Berman <Julian@GrayVines.com>
2020-05-25 10:04:08 -0400

    More Pelican metadata.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,3 +1,14 @@
+AUTHOR = "Julian Berman"
+
+SITENAME = "TIL"
 SITEURL = "https://til.grayvines.com"
+
 TIMEZONE = "America/New_York"
 DEFAULT_DATE = "fs"
+
+SOCIAL = [
+    ("@JulianWasTaken", "https://twitter.com/JulianWasTaken"),
+    ("@julianberman", "https://instagram.com/julianberman"),
+]
+GITHUB_URL = "https://github.com/Julian"
+TWITTER_USERNAME = "JulianWasTaken"
